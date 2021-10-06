48d6eeff5d0f84793bbe7880384262f5e87dca07
Mahim <mahim216@gmail.com>
2021-10-06 17:00:09 -0400

    Delete getBombSVG.js

--- modules/getBombSVG.js
+++
@@ -1,8 +0,0 @@
-function getBombSVG() {
-    const bombSVG = document.createElement('img');
-    bombSVG.setAttribute('src', '../assets/mine.svg');
-    bombSVG.classList.add('bomb');
-    return bombSVG;
-}
-
-export default getBombSVG;
