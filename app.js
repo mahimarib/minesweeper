9016c982539ed44dcf75f3c34438c5384dfd508a
Mahim <mahim216@gmail.com>
2021-10-13 18:38:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+import getAtomicValue from './modules/getAtomicValue.js';
 import Grid from './modules/Grid.js';
 
 const gridElement = document.querySelector('.grid');
 
-const grid = new Grid(gridElement, 10, 10, 20);
+const rows = 10,
+    cols = 10,
+    bombs = 20;
+
+export const flags = getAtomicValue(bombs);
+
+const [, , addHook] = flags;
+
+addHook(flags => console.log(flags));
+
+const grid = new Grid(gridElement, rows, cols, bombs);
