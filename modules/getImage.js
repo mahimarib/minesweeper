1f5067a1829a0320cca5e1d04544c2b0167d4dde
Mahim <mahim216@gmail.com>
2021-10-17 18:03:16 -0400

    making images not draggable

--- modules/getImage.js
+++ modules/getImage.js
@@ -1,6 +1,7 @@
 function getImage(path) {
     const imgElement = document.createElement('img');
     imgElement.setAttribute('src', path);
+    imgElement.setAttribute('draggable', false);
     return imgElement;
 }
 
